ef611fedf70a6efa20e51207367d18d5512ec03c
Дмитрий Лисютин <54859565+SelectionForCollection@users.noreply.github.com>
2022-06-15 19:34:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,67 @@
+import telebot
+
+bot = telebot.TeleBot('5502039945:AAFf6awspHw9bRMYk5PqObtCPQx6Pg2GdAs')
+BOT_ID = 5502039945
+CHANNEL_NAMES = []
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет, ' + message.chat.first_name + '.')
+
+@bot.message_handler(commands=['check_channels'])
+def check_channels(message):
+    response = ""
+    for channel in CHANNEL_NAMES:
+        try:
+            admins = bot.get_chat_administrators(channel)
+            for admin in admins:
+                if admin.user.id == BOT_ID:
+                    response += "В " + channel + " я админ, все ок\n"
+        except Exception:
+            response += "В " + channel + " я НЕ админ, это не гуд\n"
+            CHANNEL_NAMES.remove(channel)
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(commands=['mailing_channels'])
+def mailing_channels(message):
+    bot.send_message(message.chat.id, 'Подключенные каналы: ' + str(CHANNEL_NAMES))
+
+
+@bot.message_handler(commands=['add_mailing_channel'])
+def helper_add_mailing_channel(message):
+    send = bot.send_message(message.chat.id, 'Отправляй.\nМожешь отправить сразу несколько, только тогда пиши их с новой строки.')
+    bot.register_next_step_handler(send, add_mailing_channel)
+
+def add_mailing_channel(message):
+    buff = message.text.split('\n')
+    for el in buff:
+        CHANNEL_NAMES.append(el)
+    bot.send_message(message.chat.id, 'Подключенные каналы: ' + str(CHANNEL_NAMES))
+
+
+@bot.message_handler(commands=['new_mailing'])
+def new_mailing(message):
+    send = bot.send_message(message.chat.id, 'Cлушаю')
+    bot.register_next_step_handler(send, remail)
+
+def remail(message):
+    for channel in CHANNEL_NAMES:
+        match message.content_type:
+            case 'text':
+                bot.send_message(channel, message.text)
+            case 'photo':
+                bot.send_photo(channel, message.photo[-1].file_id, caption=message.caption)
+            case 'video':
+                bot.send_video(channel, message.video.file_id, caption=message.caption)
+            case 'voice':
+                bot.send_audio(channel, message.voice.file_id)
+            case 'audio':
+                bot.send_audio(channel, message.audio.file_id, caption=message.caption)
+            case 'document':
+                bot.send_document(channel, message.document.file_id, caption=message.caption)
+            case 'sticker':
+                bot.send_sticker(channel, message.sticker.file_id)
+    bot.send_message(message.chat.id, 'Отправил, епта')
+
+bot.polling(none_stop=True)
